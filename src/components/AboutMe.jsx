4b377a101b13e6c3b142f66361b2e8eab3a22df7
chilya-coder <yuliya.chimyrys@gmail.com>
2025-10-29 16:47:05 +0000

    Skeleton UI added

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,23 @@
+import Typewriter from 'typewriter-effect';
+import { PERSONAL_INFO, TYPEWRITER_CONFIG } from '../constants/strings';
+
+export default function AboutMe() {
+  return (
+    <section className="about-me">
+      <img src="/yuchi.jpeg" alt="Yuchi profile" className="profile-photo" />
+      <div className="about-text">
+        <h1>{PERSONAL_INFO.greeting}</h1>
+        <h2>
+          I'm{' '}
+          <Typewriter
+            options={{
+              strings: PERSONAL_INFO.titles,
+              ...TYPEWRITER_CONFIG,
+            }}
+          />
+        </h2>
+        <p>{PERSONAL_INFO.description}</p>
+      </div>
+    </section>
+  );
+}
---
+++ src/constants/strings.js
@@ -0,0 +1,14 @@
+export const PERSONAL_INFO = {
+  name: 'Yuchi',
+  greeting: 'Hey!',
+  titles: ['Yuliia Chimyrys', 'Yuchi', '@insomniacengineer'],
+  description:
+    "I'm Software Engineer@Microsoft passionate about learning, creativity and distributed systems.",
+};
+
+export const TYPEWRITER_CONFIG = {
+  autoStart: true,
+  loop: true,
+  delay: 75,
+  deleteSpeed: 50,
+};
